468ae054415c3e4dfc349fecd04b0dd9f6e198c5
Ghada Ahmed <ghadaelza72@gmail.com>
2025-01-09 04:05:42 +0200

    .

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useContext} from 'react'
 import style from '../Home/Home.module.scss'
 import { Link } from 'react-router-dom';
 import { MediaContext } from '../Context/MediaStore';
