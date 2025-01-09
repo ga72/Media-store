f84f9b7ec99a5ff710b883665dcfb772c014687d
Ghada Ahmed <ghadaelza72@gmail.com>
2025-01-09 15:40:10 +0200

    validation test

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -25,7 +25,7 @@ export default function LogIn({saveUserData}) {
   }
   let validateDataForm =()=>{
     const schema = Joi.object({
-      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,20}$')),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*]{8,20}$')),
       email: Joi.string().email({ tlds: { allow: ['com', 'net'] } })
   })
   return schema.validate(user , {abortEarly: false});
